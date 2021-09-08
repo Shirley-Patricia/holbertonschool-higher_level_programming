985d59dbb0339d3168cbf108509723b448fa82a3
Shirley <shircar16@gmail.com>
2021-09-07 23:27:30 -0500

    fix code for -1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,6 +9,6 @@ else:
 if l_d > 5:
     print("{} {} is {} and is greater than 5".format(l, number, l_d))
 elif l_d == 0:
-    print("{} {} is {} and is 0".format(number, l_d))
+    print("{} {} is {} and is 0".format(l, number, l_d))
 else:
     print("{} {} is {} and is less than 6 and not 0".format(l, number, l_d))
