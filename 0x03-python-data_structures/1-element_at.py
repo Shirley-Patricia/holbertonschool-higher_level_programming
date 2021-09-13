1440efa5aab340a5773899126454cacb2919ed90
Shirley <shircar16@gmail.com>
2021-09-13 17:25:16 -0500

    identation

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-	for i in range(len(my_list)):
-		if idx < 0:
-			return None
-		elif idx > len(my_list):
-			return None
-		elif my_list[i] == idx:
-			return my_list[idx]
+    for i in range(len(my_list)):
+        if idx < 0:
+            return None
+        elif idx > len(my_list):
+            return None
+        elif my_list[i] == idx:
+            return my_list[idx]
