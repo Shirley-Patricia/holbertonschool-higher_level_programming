9eed2e550c5a2286cc9fc8ef805d556b9e91ae47
Shirley <shircar16@gmail.com>
2021-09-14 18:31:23 -0500

    adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-	
-	if tuple_a[i] < 2 or tuple_b < 2
+    
+    #if tuple_a[i] < 2 or tuple_b < 2:
 
-	sum = tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
-	return sum
+    sum = tuple(map(lambda x, y: x + y, tuple_a, tuple_b))
+    return sum
+    
