0497a44cede75e366024ab4c564f36d96c12b98f
Shirley <shircar16@gmail.com>
2021-09-14 22:11:01 -0500

    fix string empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-	l = len(sentence)
-	a = sentence[0]
-	if sentence == ():
-		return (l, None)
+	if sentence == "":
+		return (0, None)
 	else:
+		l = len(sentence)
+		a = sentence[0]
 		return (l, a)
