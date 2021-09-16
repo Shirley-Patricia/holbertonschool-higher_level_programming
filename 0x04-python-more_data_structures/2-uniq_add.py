6f0f8546c8ec6bc2fce1a2cb3c7e9f7d67974e46
Shirley <shircar16@gmail.com>
2021-09-15 23:22:03 -0500

    add unique integer in a list

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+	my_list.sort()
+	add = my_list[0]
+	for i in range(0, (len(my_list)-1)):
+		if my_list[i] != my_list[i + 1]:
+			add += my_list[i + 1]
+	return add
