1303b72b56b9a3cab6de7023206eb5e9192cd2cb
Shirley <shircar16@gmail.com>
2021-09-15 23:27:12 -0500

    condition if list is empty

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-	my_list.sort()
-	add = my_list[0]
-	for i in range(0, (len(my_list)-1)):
-		if my_list[i] != my_list[i + 1]:
-			add += my_list[i + 1]
-	return add
+    if my_list == []:
+        return None
+    my_list.sort()
+    add = my_list[0]
+    for i in range(0, (len(my_list)-1)):
+        if my_list[i] != my_list[i + 1]:
+            add += my_list[i + 1]
+    return add
