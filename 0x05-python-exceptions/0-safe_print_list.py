96be86d67ea0b8af849f35b8a5ac7cf22bb518c4
Shirley <shircar16@gmail.com>
2021-09-27 15:37:40 -0500

    print list using try and except

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/bin/bash/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(0, x):
+            print("{:d}".format(my_list[i]), end="")
+        print()
+        return x	
+    except:
+        print()
+        pass
+    return i
