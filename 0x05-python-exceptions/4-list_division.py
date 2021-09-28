c4f46ead121220f348df9a4b1fd69e75f04bfabe
Shirley <shircar16@gmail.com>
2021-09-27 23:24:17 -0500

    change return

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,9 +2,7 @@
 def list_division(my_list_1, my_list_2, list_length):
     try:
         a = [i / j for i, j in zip(my_list_1, my_list_2)]
-        if my_list_1 < list_length or my_list_2 < list_length:
-            print("out of range") 
-        return [a]
+        return a
     except TypeError:
         print("wrong type")
     except ZeroDivisionError:
