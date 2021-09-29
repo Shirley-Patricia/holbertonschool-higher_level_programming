3a75953f64c19af5430243b6172ad76178854ee7
Shirley <shircar16@gmail.com>
2021-09-29 15:48:30 -0500

    property and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Define class"""
+
+
+class Square:
+    """Class square defined"""
+    def __init__(self, size=0):
+        """defined method"""
+        self.__size = size
+        """Private instance attribute"""
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """square area method"""
+        return (self.__size ** 2)
