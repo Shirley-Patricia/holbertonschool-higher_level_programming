5cfea2173e17baf269f04bb4839d3de3e65e3d7e
Shirley <shircar16@gmail.com>
2021-09-28 22:57:12 -0500

    defining private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define class"""
+
+
+class Square:
+    """Class square defined"""
+    def __init__(self, size):
+        """defined method"""
+        self.__size = size
+        """Private instance attribute"""
