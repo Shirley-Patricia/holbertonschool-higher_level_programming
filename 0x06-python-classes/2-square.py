cb9983fce97af98d1f19720684ac1173e716015c
Shirley <shircar16@gmail.com>
2021-09-28 23:45:19 -0500

    typeError anValueError

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define class"""
+
+
+class Square:
+    """Class square defined"""
+    def __init__(self, size=0):
+        """defined method"""
+        self.__size = size
+        """Private instance attribute"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
