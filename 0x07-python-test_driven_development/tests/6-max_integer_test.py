13daa9a5b9205cb486166d2932c37294d98087d5
Shirley <shircar16@gmail.com>
2021-10-06 23:45:01 -0500

    unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,15 +4,28 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
 
     def test_max_integer(self):
-        result = 8
-        self.assertEqual(result.max_integer(), [2, 4, 8, 6])
         # Check that result.max_integer fail when function run
-        with self.assertRaises(TypeError):
-            result.max_integer(2)
+        self.assertAlmostEqual(max_integer([2, 4, 8, 6]), 8)
 
+    def test_only_negative_numbers(self):
+        self.assertAlmostEqual(max_integer([-1, -3, -5, -7]), -1)
+
+    def test_negative_and_positive_numbers(self):
+        self.assertAlmostEqual(max_integer([10, -40, 90, 50]), 90)
+
+    def test_floats(self):
+        self.assertAlmostEqual(max_integer([1.5, 3.4, 2, 0.5]), 3.4)
+
+    def test_empty_list(self):
+        self.assertAlmostEqual(max_integer([]), None)
+
+    def test_one_character(self):
+        """test with one character"""
+        self.assertRaises(TypeError, max_integer, [2, "g", 8, 6])
 
 if __name__ == '__main__':
     unittest.main()
