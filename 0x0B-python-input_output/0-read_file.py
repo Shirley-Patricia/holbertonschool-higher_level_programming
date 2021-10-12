c358a042d8baa9a06bf35d1158b9f85ade191918
Shirley <shircar16@gmail.com>
2021-10-12 17:38:36 -0500

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-"""function read a text file"""
+"""function read a text file
+with open and print in stdout
+"""
 
 
 def read_file(filename=""):
-	with open(filename, 'r') as file:
-		print(file.read())
+    """using open to read file"""
+    with open(filename, 'r') as file:
+        print(file.read())
