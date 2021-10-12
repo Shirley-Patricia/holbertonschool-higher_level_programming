cd480bfcd286cb8800f7c9610049deaff42dbf7c
Shirley <shircar16@gmail.com>
2021-10-11 21:57:13 -0500

    change to type

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,9 +5,9 @@ of the specified class ; otherwise False
 
 
 def is_same_class(obj, a_class):
-    if a_class is object:
-        return False
-    if isinstance(obj, a_class):
+    """compare object with a specific class"""
+    
+    if type(obj) is a_class:
         return True
     else:
         return False
