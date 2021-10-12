b4646544159b7b43ab4441dc41c5c84a3393e512
Shirley <shircar16@gmail.com>
2021-10-11 20:02:21 -0500

    add class list

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """function that return attributes and methods of an object"""
 
+class list:
+    pass
 
-def lookup(obj):
-	return dir(obj)
+    def lookup(obj):
+        return dir(obj)
