1cd22859b916bcc47a0c37f27447c88b479146c9
Shirley <shircar16@gmail.com>
2021-10-11 23:02:30 -0500

    no area implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Empty class base geometry"""
+
+
+class BaseGeometry:
+    """class base geometry"""
+    
+    def area(self):
+        raise Exception("area() is not implemented")
