71344324f38231debc649b0955b53e99cff37860
Shirley <shircar16@gmail.com>
2021-10-11 22:17:01 -0500

    change documentation

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""function returns True if the object is exactly an instance
-of the specified class ; otherwise False
+"""returns True if the object is an instance of, 
+or if the object is an instance of a class that inherited from, 
+the specified class; otherwise False.
 """
 
 
