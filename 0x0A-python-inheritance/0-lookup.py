e746e89b1c1c2284f206ec7c21c9d7b430027410
Shirley <shircar16@gmail.com>
2021-10-11 21:36:15 -0500

    fix parenthesis

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """function that return attributes and methods of an object"""
 
-class list:
-    pass
 
-    def lookup(obj):
-        return dir(obj)
+def lookup(obj):
+    """dir list attributes and methods of an object"""
+    return (dir(obj))
