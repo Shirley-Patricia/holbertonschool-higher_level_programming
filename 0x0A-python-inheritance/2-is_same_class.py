bc0d67b730221bed03c5d53da42e05bb78bc5da4
Shirley <shircar16@gmail.com>
2021-10-11 21:24:35 -0500

    add conditinal a-class-object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""function returns True if the object is exactly an instance 
+"""function returns True if the object is exactly an instance
 of the specified class ; otherwise False
 """
 
 
 def is_same_class(obj, a_class):
+    if a_class is object:
+        return False
     if isinstance(obj, a_class):
         return True
     else:
