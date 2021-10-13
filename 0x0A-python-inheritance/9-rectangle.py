8ca0d2a133d5dee3aa5575e1c3fb5dc8cb2043a7
Shirley <shircar16@gmail.com>
2021-10-13 16:53:28 -0500

    area of the rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""class Rectangle that inheritage of BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class Rectangle that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        super().area
+        self.area = self.__width * self.__height
+        return ("{}".format(self.area))
