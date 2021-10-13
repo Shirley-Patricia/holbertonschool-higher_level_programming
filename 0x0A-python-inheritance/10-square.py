ed58678cb9666753fb8fde3afa5814dfdd5d1990
Shirley <shircar16@gmail.com>
2021-10-13 18:50:42 -0500

    subclass square fix doc

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """Class Square that inherits from Rectangle"""
 
+
 Rectangle = __import__('9-rectangle').Rectangle
 
-class Square(Rectangle):
 
+class Square(Rectangle):
+    """subclass of rectangle"""
     def __init__(self, size):
+        """sub method"""
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """Class Square that inherits from Rectangle"""
 
+
 Rectangle = __import__('9-rectangle').Rectangle
 
-class Square(Rectangle):
 
+class Square(Rectangle):
+    """subclass of rectangle"""
     def __init__(self, size):
+        """sub method"""
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
     def __str__(self):
         return "[Square] {}/{}".format(self.__size, self.__size)
-
-    
