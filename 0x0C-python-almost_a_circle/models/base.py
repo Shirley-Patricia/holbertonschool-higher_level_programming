c564f499b5479d410a3bf332cdda6134d9657f35
Shirley <shircar16@gmail.com>
2021-10-18 22:00:37 -0500

    JSON string representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -37,7 +37,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries == []:
+        if list_dictionaries == [] or list_dictionaries is None:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
