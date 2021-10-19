8b78a7b6f4b0b09ad952d277eb554f331710e676
Shirley <shircar16@gmail.com>
2021-10-19 10:36:19 -0500

    test class base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -8,22 +8,22 @@ from models.base import Base
 class Test_Base(unittest.TestCase):
 
     def test_none_id(self):
+        Base._Base__nb_objects = 0
         f = Base()
-        print("----->", f.id)
-        self.assertEqual(f.id, 1)
+        self.assertEqual(f.id, 1, "if fail is an error")
 
     def test_instance_(self):
         # Check the instance
         b = Base(56)
         self.assertIsInstance(b, Base)
 
-    def id_equal(self):
+    def test_id_equal(self):
         c = Base(5)
         self.assertEqual(c.id, 5)
 
     def test_no_object(self):
-        a = 12
-        self.assertIsInstance(a, Base, "this test is not correct")
+        a = Base("g")
+        self.assertIsInstance(a, Base)
          
     def test_id_int(self):
         g = Base()
@@ -33,6 +33,11 @@ class Test_Base(unittest.TestCase):
         d = Base()
         e = Base()
         self.assertNotEqual(d.id, e.id)
+
+    def test_id_equal(self):
+        d = Base(4)
+        e = Base(4)
+        self.assertEqual(d.id, e.id)
 
 if __name__ == '__main__':
     unittest.main()
