ac23f122605c294e3f32675cc5905e28836c4212
Shirley <shircar16@gmail.com>
2021-10-19 11:12:09 -0500

    from_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -51,6 +51,6 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         if json_string is None or len(json_string) == 0:
-            return "[]"
+            return list([])
         else:
             return json.loads(json_string)
