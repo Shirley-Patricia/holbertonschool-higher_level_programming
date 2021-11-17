969b47910fb175a2497e68e7a4d2c12f541bdb9d
Shirley <shircar16@gmail.com>
2021-11-17 00:23:44 -0500

    update the score

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- Datebases SQL
+-- This script updates the score of Bob to 10 in the table second_table (database hbtn_0c_0) in MySQL server
+UPDATE second_table SET score = 10 WHERE name = "Bob";
