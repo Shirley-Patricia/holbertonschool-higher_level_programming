c319ad7f3188e077b0cb68e291b9b93a45563c50
Shirley <shircar16@gmail.com>
2021-11-17 01:41:52 -0500

    Say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- Say my name
 -- This script lists all records of the table second_table (database hbtn_0c_0)
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
