2bc377571f9b2bee105ac91c0644407a8603dc0e
Shirley <shircar16@gmail.com>
2021-11-17 00:18:11 -0500

    fix comment

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- Datebases SQL
--- This script lists all rows of the table first_table from the database hbtn_0c_0 in MySQL server
+-- This script lists all records of the table second_table from the database hbtn_0c_0 in MySQL server
 SELECT score, name FROM second_table ORDER BY score DESC;
