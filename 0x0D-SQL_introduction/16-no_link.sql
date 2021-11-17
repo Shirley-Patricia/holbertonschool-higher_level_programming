7079a18c23889562d27b83fd168321945b8378b0
Shirley <shircar16@gmail.com>
2021-11-17 01:37:27 -0500

    Say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,3 @@
 -- Say my name
 -- This script lists all records of the table second_table (database hbtn_0c_0)
 SELECT score, name FROM second_table ORDER BY score DESC;
-DELETE FROM second_table WHERE name = George
-INSERT INTO second_table VALUES(5, "Aria", 18);
-INSERT INTO second_table VALUES(6, "Aria", 12);
