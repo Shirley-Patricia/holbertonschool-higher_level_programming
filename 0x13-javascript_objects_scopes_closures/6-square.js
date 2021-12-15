ea86354020bad1adace08391d7e20dd05e733ac5
Shirley <shircar16@gmail.com>
2021-12-15 11:34:32 -0500

    print square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,7 +7,16 @@ module.exports = class Square extends Rectangle {
     super(size, size);
   }
 
-  charPrint (c) {
-    super.print();
+  charPrint (c = 'X') {
+    let size;
+    let j;
+
+    for (size = 0; size < this.height; size++) {
+      let row = '';
+      for (j = 0; j < this.width; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
   }
 };
