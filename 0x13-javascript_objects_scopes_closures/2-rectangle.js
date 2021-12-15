f0842bcb1955f4f2f5a97bc6227ba20673872981
Shirley <shircar16@gmail.com>
2021-12-14 22:35:59 -0500

    empty object and print rectangle

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return 0;
+    } else if (!w || !h) {
+      return 0;
+    }
+    this.width = w;
+    this.height = h;
+  }
+};
