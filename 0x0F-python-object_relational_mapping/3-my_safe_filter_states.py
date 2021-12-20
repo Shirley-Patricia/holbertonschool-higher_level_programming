2d9a19ce6956e300dce2099c56a27571ba11cce0
Shirley <shircar16@gmail.com>
2021-12-20 12:19:16 -0500

    SQL injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""list all states from database hbtn_0e_0_usa
+"""
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], database=sys.argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("""
+        SELECT * FROM states WHERE name= %(name)s
+        ORDER BY id ASC""", {'name': sys.argv[4]})
+
+    result = cur.fetchall()
+    for x in result:
+        print(x)
+
+    db.close()
