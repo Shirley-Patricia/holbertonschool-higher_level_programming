1735c3b778f3ee5409ee1532a76566117b477903
Shirley <shircar16@gmail.com>
2021-12-20 17:45:21 -0500

    creating of State table and query

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,8 +10,9 @@ if __name__ == "__main__":
     from sqlalchemy.orm import sessionmaker
     from model_state import Base, State
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
-                           sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.
+                           argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
     session = Session()
