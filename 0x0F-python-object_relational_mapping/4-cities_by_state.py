3a2d06c86ba1ef4f41508da6f1eea5053ff42ead
Shirley <shircar16@gmail.com>
2021-12-20 10:42:16 -0500

    cities by state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id=states.id ORDER BY id ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+    INNER JOIN states ON cities.state_id=states.id ORDER BY id ASC")
 
     result = cur.fetchall()
     for x in result:
