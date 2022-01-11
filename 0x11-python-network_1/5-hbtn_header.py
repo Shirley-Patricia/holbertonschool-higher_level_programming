6560058791a97b9fe70ff49fbc2a1a0398cc3059
Shirley <shircar16@gmail.com>
2022-01-11 18:03:55 -0500

    adding get in print

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,9 +4,8 @@
    the variable X-Request-Id in the response header
 """
 
-import requests
-import sys
-
 if __name__ == "__main__":
+    import requests
+    import sys
     r = requests.get(sys.argv[1])
-    print(r.headers["X-Request-Id"])
+    print(r.headers.get["X-Request-Id"])
