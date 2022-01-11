a8f3b29eb04b8f5d0bccf810be6ae433f18bf5aa
Shirley <shircar16@gmail.com>
2022-01-10 20:08:00 -0500

    fixing comment

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# Write a Python script that fetches https://intranet.hbtn.io/status
+"""Script that takes in a URL, sends a request to the URL and
+   displays the body of the response (decoded in utf-8).
+"""
 
 from urllib import request
 import urllib.Error
