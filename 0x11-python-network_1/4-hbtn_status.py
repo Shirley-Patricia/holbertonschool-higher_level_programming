c30e8f3913e1f58feb47ff4e0118a7ed071146a8
Shirley <shircar16@gmail.com>
2022-01-10 22:49:38 -0500

    pycodestyle

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-# Write a Python script that fetches https://intranet.hbtn.io/status
+"""Write a Python script that fetches https://intranet.hbtn.io/status
+   using the package requests.
+"""
 
 import requests
 
 if __name__ == "__main__":
-	r = requests.get('https://intranet.hbtn.io/status')
-	print("Body response:")
-	print("\t- type: {}".format(type(r.text)))
-	print("\t- content: {}".format(r.text))
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
