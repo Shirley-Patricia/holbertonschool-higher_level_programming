f915c37ea09deca8e4e804b5c1ddaff4628dd539
Shirley <shircar16@gmail.com>
2022-01-11 22:10:06 -0500

    response without \n

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a URL and displays only the status code of the response, without use pipe, redirection, ; and &&.
-curl --write-out '%{http_code}\n' -s --no-keepalive --output /dev/null $1
+curl --write-out '%{http_code}' -s --no-keepalive --output /dev/null $1
