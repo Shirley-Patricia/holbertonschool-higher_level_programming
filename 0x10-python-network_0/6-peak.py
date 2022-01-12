a1a5aa056ab79da204eb7bbd2e4ddfad2f13902b
Shirley <shircar16@gmail.com>
2022-01-11 19:25:39 -0500

    peak of a list

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function that finds a peak in a list of unsorted integers"""
+
+def find_peak(list_of_integers):
+	"""find the max number in a list"""
+	if list_of_integers != []:
+		list_of_integers.sort()
+		return list_of_integers[-1]
