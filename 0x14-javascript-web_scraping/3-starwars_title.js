134e1431048b579adc47145bb06213637c973533
Shirley <shircar16@gmail.com>
2022-02-02 12:55:45 -0500

    changing body to const bod

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,6 +6,6 @@ const url = ('https://swapi-api.hbtn.io/api/films/' + argv[2]);
 
 request(url, (error, response, body) => {
   if (error) console.log(error);
-  body = JSON.parse(body);
-  console.log(body.title);
+  const bod = JSON.parse(body);
+  console.log(bod.title);
 });
