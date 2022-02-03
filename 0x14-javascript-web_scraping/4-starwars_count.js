41e961312c7f060235fe71b51a0e65ee04e6cb85
Shirley <shircar16@gmail.com>
2022-02-02 19:33:26 -0500

    adding argv

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,17 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = ('https://swapi-api.hbtn.io/api/films/');
+const { argv } = require('process');
+const url = argv[2];
 
 request(url, (error, response, body) => {
   if (error) console.log(error);
   const bodies = JSON.parse(body);
 
   let count = 0;
-  let i;
-  let j;
-
-  for (i in bodies.results) {
+  for (let i in bodies.results) {
     const len = bodies.results[i].characters.length;
-    for (j = 0; j < len; j++) {
+    for (let j = 0; j < len; j++) {
       const sentence = bodies.results[i].characters[j];
       if (sentence.includes('/18/')) {
         count++;
