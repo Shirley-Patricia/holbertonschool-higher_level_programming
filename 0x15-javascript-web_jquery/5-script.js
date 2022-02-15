3b511a8ed80e024ab0aa0551d495769fbac2597c
Shirley-Patricia <shircar16@gmail.com>
2022-02-15 11:01:24 -0500

    adds a <li> element to a list

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/** JavaScript script that adds a <li> element to a
+* list when the user clicks on the tag DIV#add_item
+*/
+
+$(document).ready(function () {
+  $('#add_item').on('click', function () {
+    $('.my_list').append('<li>Item</li>');
+  });
+});
